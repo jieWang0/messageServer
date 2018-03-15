b1488906971b0d089e4aa118f9138be14d4b0e25
jieWang <jie.wang@transwarp.io>
2018-03-15 14:10:05 +0800

    删除无用代码

--- gn-tdc-server/src/main/java/io.transwarp.tdc.notif.server/kafkanotification/service/INgConsumerService.java
+++
@@ -1,7 +0,0 @@
-package io.transwarp.tdc.notif.server.kafkanotification.service;
-
-import java.util.Properties;
-
-public interface INgConsumerService {
-    public Properties consumeDetail();
-}
--- gn-tdc-server/src/main/java/io.transwarp.tdc.notif.server/kafkanotification/service/INgProducerService.java
+++
@@ -1,9 +0,0 @@
-package io.transwarp.tdc.notif.server.kafkanotification.service;
-
-import java.util.Properties;
-
-public interface INgProducerService {
-
-    public void produce(Properties properties, String topic,String key, String message);
-    public void tryProduce(Properties properties, String topic, String key,String message);
-}
